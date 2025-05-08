156d51add1465b4199626c90c171c1a912d46c6b
rouhi <r.shafahi11@gmail.com>
2025-05-09 01:35:43 +0330

    better url for token

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -10,7 +10,7 @@ app_name = 'api-v1'
 urlpatterns = [
     path('signup', views.SignupApiView.as_view(), name='signup'),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
