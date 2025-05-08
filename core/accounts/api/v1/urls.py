c68d98ccc39b20470b7356688a5cab3fb34e90f4
rouhi <r.shafahi11@gmail.com>
2025-05-09 01:02:25 +0330

    implement user registration with email uniqueness validation

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'api-v1'
 urlpatterns = [
-    path('login', views.login_api_view, name='login')
+    path('Signup', views.SignupApiView.as_view(), name='Signup')
 ]
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,4 +1,12 @@
+from rest_framework.views import APIView, Response
+from rest_framework import generics, status
+from .serializers import SignupSerializer
 
+class SignupApiView(generics.GenericAPIView):
+    serializer_class = SignupSerializer
 
-def login_api_view(request):
-    pass
+    def post(self, request, *args, **kwargs):
+        serializer = SignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
